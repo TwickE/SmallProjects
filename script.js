7a2cc2e13f7523b8827abbad12d0aa076a98b0eb
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2022-12-01 23:44:27 +0000

    Added Small Projects home page

---
+++ script.js
@@ -0,0 +1,30 @@
+const card1 = document.getElementById('card1');
+const card2 = document.getElementById('card2');
+const card3 = document.getElementById('card3');
+const card4 = document.getElementById('card4');
+const card5 = document.getElementById('card5');
+const card6 = document.getElementById('card6');
+
+card1.addEventListener('click', function() {
+    window.location.href = '/CountdownTimer/index.html';
+});
+
+card2.addEventListener('click', function() {
+    window.location.href = '/QuizApp/index.html';
+});
+
+card3.addEventListener('click', function() {
+    window.location.href = '/RecipeApp/index.html';
+});
+
+card4.addEventListener('click', function() {
+    window.location.href = '/NotesApp/index.html';
+});
+
+card5.addEventListener('click', function() {
+    window.location.href = '/TodoApp/index.html';
+});
+
+card6.addEventListener('click', function() {
+    window.location.href = '/MovieApp/index.html';
+});
