db547d8908f581eadad8deb53ddde0b00bf8c897
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-02-21 20:25:45 +0000

    Made some progress in the app

--- GithubProfilesApp/script.js
+++ GithubProfilesApp/script.js
@@ -1,5 +1,9 @@
 const APIURL = "https://api.github.com/users/";
 
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+
 async function getUser(user) {
     const resp = await fetch(APIURL + user);
     const respData = await resp.json();
@@ -8,5 +12,32 @@ async function getUser(user) {
 }
 
 function createUserCard(user) {
+    const cardHTML = `
+        <div class="card">
+            <img src="${user.avatar_url}" alt="${user.name}"/>
+            <div class="card-info">
+                <h2>${user.name}</h2>
+                <p>${user.bio}</p>
+                <ul>
+                    <li>Followers: ${user.followers}</li>
+                    <li>Following: ${user.following}</li>
+                    <li>Repos: ${user.public_repos}</li>
+                </ul>
+            </div>
+        </div>
+    `;
 
+    main.innerHTML = cardHTML;
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = search.value;
+
+    if (user) {
+        getUser(user);
+
+        search.value = "";
+    }
+});
