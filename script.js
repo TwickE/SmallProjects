69ce1cbc9782845e8c9d1242b0c0761c7cf3657e
Frederico Flores Moita da Silva <118013263+TwickE@users.noreply.github.com>
2023-02-22 19:31:26 +0000

    Simplified the main page Javascript

--- script.js
+++ script.js
@@ -1,45 +1,10 @@
-const card1 = document.getElementById('card1');
-const card2 = document.getElementById('card2');
-const card3 = document.getElementById('card3');
-const card4 = document.getElementById('card4');
-const card5 = document.getElementById('card5');
-const card6 = document.getElementById('card6');
-const card7 = document.getElementById('card7');
-
-card1.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/CountdownTimer/index.html';
-});
-
-card2.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/QuizApp/index.html';
-});
-
-card3.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/RecipeApp/index.html';
-});
-
-card4.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/NotesApp/index.html';
-});
-
-card5.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/TodoApp/index.html';
-});
-
-card6.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/MovieApp/index.html';
-});
-
-card7.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/GithubProfilesApp/index.html';
-});
-
-card7.addEventListener('click', function() {
-    window.location.href = '/SmallProjects/DrawingApp/index.html';
-});
-
-/* const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card');
 
 cards.forEach(card => {
-    
-}); */
+    card.addEventListener('click', function() {
+        const img = card.querySelector('img');
+        const imagSrc = img.src;
+        const imgName = imagSrc.split('/').pop().split('.')[0];
+        window.location.href =  `/SmallProjects/${imgName}/index.html`;
+    });
+});
